dd95296b359040dc6ca96a3eeed219da06db9082
Antoine Durieux <adurieux@stanford.edu>
2012-03-10 11:17:17 +0100

    Changed implementation to interface (bis).

--- Tests/Templating/Helper/FacebookHelperTest.php
+++ Tests/Templating/Helper/FacebookHelperTest.php
@@ -42,7 +42,7 @@ class FacebookHelperTest extends \PHPUnit_Framework_TestCase
             ))
             ->will($this->returnValue($expected));
 
-        $routing = $this->getMockBuilder('Symfony\Component\Routing\Generator\UrlGenerator')
+        $routing = $this->getMockBuilder('Symfony\Component\Routing\Generator\UrlGeneratorInterface')
             ->disableOriginalConstructor()
             ->getMock();
         $routing
